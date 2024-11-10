e09d8f1a028af6171d29f1f9a43b6c38f00c99dc
Akki-Y <120158433+Akki-Y@users.noreply.github.com>
2024-11-10 13:00:17 +0530

    Modified code

--- find_missing_number.rb
+++ find_missing_number.rb
@@ -1,8 +1,18 @@
-def find_missing_number(nums)
- n = nums.length
- total_sum = (n * (n+1))/2
- actual_sum = nums.sum
- total_sum - actual_sum
+# finding a single missing number
+arr = [1, 3, 4]
+
+def find_missing_number(arr)
+ n = arr.length + 1  # find total length & 1 is for missing number 
+ # n = arr.max  #use this for  lare nmber of array
+ 
+ expected_sum = (n * (n+1))/2     
+ sum_of_arr = arr.sum
+ missing_number = expected_sum - sum_of_arr
+ return missing_number
 end
 
-puts find_missing_number([0, 1, 3])  # 2
+puts find_missing_number(arr)  # 2
+
+# Expected sum = (4 * (4+1) /2) = 10
+# Sum of array = 8
+# Missing number = 10 - 8 = 2
